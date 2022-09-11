324275c094858dee3c8684f4c8572c3940c1ed7e
Wiliam Sanchez <william93sb@gmail.com>
2022-09-11 16:23:21 -0500

    proyecto terminado

---
+++ script.js
@@ -0,0 +1,9 @@
+import { addTask } from "./components/addTask.js";
+import { displayTasks } from "./components/readTasks.js";
+
+const btn = document.querySelector('[data-form-btn]');
+
+//Arrow functions o funciones anónimas
+btn.addEventListener('click', addTask);
+
+displayTasks();
